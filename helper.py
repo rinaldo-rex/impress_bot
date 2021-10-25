ff339fa67126d3d849bb3a51528be1d534b6a5a5
Rinaldo Rex <rinaldorexg@gmail.com>
2021-10-25 15:40:07 +0530

    Add helper function to fetch joke

---
+++ helper.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def get_joke(category="stupid"):
+    """Return a JSON representation of a joke based on a given category
+
+    Args:
+        category (str, optional): One of "stupid", "dumb", "fat". Defaults to "stupid".
+    """
+
+    joke_json = requests.get(f"https://v2.jokeapi.dev/joke/Any?contains={str(category)}")
+
+    if joke_json.status_code == 200:
+        # Send a joke only if the request was successful.
+        return joke_json.json()
+    else:
+        return {
+            "joke": "Sorry, we couldn't find a joke. Please try again'"
+        }
