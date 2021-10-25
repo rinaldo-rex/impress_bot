ad772c6ee525bb96151235e238e837f529a0fd96
Rinaldo Rex <rinaldorexg@gmail.com>
2021-10-25 15:56:59 +0530

    Update helper and bot.py

---
+++ README.md
@@ -0,0 +1,13 @@
+A simple telegram bot written to fetch jokes based on the given category.
+
+### How to run
+
+1. Clone the repo and get inside the root directory of your clone.
+2. Run `pip install -r requirements.txt`
+3. Run the following command.
+    `python bot.py`
+
+
+### Known bugs
+
+
--- helper.py
+++ helper.py
@@ -9,6 +9,7 @@ def get_joke(category="stupid"):
     """
 
     joke_json = requests.get(f"https://v2.jokeapi.dev/joke/Any?contains={str(category)}")
+    # joke_json = requests.get(f"https://dad-jokes.p.rapidapi.com/joke/search?term={str(category)}")
 
     if joke_json.status_code == 200:
         # Send a joke only if the request was successful.
